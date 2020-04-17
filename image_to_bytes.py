7e0652a4ffbd848e58ea44d5417ea3c150e9d986
cigarplug <msainani@gmail.com>
2020-04-17 10:15:51 +1000

    method for converting image into bytes-io object

--- image_to_bytes.py
+++ image_to_bytes.py
@@ -1,6 +1,5 @@
 import io
 
-
 # perpare image for html transfer
 # reference: https://towardsdatascience.com/python-plotting-api-expose-your-scientific-python-plots-through-a-flask-api-31ec7555c4a8
 

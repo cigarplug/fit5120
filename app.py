980e480c5bfef5bd8791bcb3e80a2a60b5cf19e1
cigarplug <msainani@gmail.com>
2020-04-25 17:20:41 +1000

    test googlemaps-flask

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, render_template
 from db import api
 import mysql.connector
 import os
 from pvt.test_report import report
-import folium
+from flask_googlemaps import GoogleMaps, Map
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="maps_template")
 app.config["DEBUG"] = True
+app.config['GOOGLEMAPS_KEY'] = os.environ["gcp_key"]
+GoogleMaps(app)
 
 
 
 @app.route('/crashes_tod_atm', methods=[ 'POST'])
 def crashes_tod_atm():
 	content = request.json
-	lat = float(content["lat"])
-	lon = float(content["lon"])
-	# kms = float(content["kms"])
 
-	tod_atm = api( lat, lon).crash_tod_atm()
-	return send_file(tod_atm, attachment_filename='plot.png', mimetype='image/png')
+	try:
+		# extract user gps coordinates
+		lat = float(content["lat"])
+		lon	= float(content["lon"])
+		# kms = float(content["kms"])
+
+		# create plot 
+		tod_atm = api( lat, lon).crash_tod_atm()
+		return send_file(tod_atm, attachment_filename='plot.png', mimetype='image/png')
+	
+	except KeyError:
+		# if lat or lon data is not received in client request
+		return jsonify({"res": "invalid/missing data"})
+
+
+	
  
 
 
@@ -70,10 +83,35 @@ def pvt_data(type):
 
 
 @app.route('/map', methods = ["GET", "POST"])
-def index():
-    start_coords = (-37.8767985, 144.9882031)
-    folium_map = folium.Map(location=start_coords, zoom_start=16)
-    return folium_map._repr_html_()
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1419,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 37.4300,
+             'lng': -122.1400,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    return render_template('example.html', mymap=mymap, sndmap=sndmap)
+    
 
 
 
